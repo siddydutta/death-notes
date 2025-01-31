63df944f8b9ba73635957592c1f4803c86e2b9b4
Siddhartha Pratim Dutta <2897074D@student.gla.ac.uk>
2025-01-31 16:04:46 +0000

    add logging

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,8 +1,13 @@
+import logging
+
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import redirect
 from django.views import View
 
 from auth_app.clients.microsoft import REDIRECT_URI, SCOPES, get_user_info, msal_app
+
+
+logger = logging.getLogger(__name__)
 
 
 class MSLoginView(View):
@@ -19,6 +24,7 @@ class MSLoginCallbackView(View):
     def get(self, request, *args, **kwargs):
         code = request.GET.get('code')
         if code is None:
+            logger.warning('[MICROSOFT CALLBACK] No code in request')
             return HttpResponseBadRequest()
         result = msal_app.acquire_token_by_authorization_code(
             code=code,
@@ -27,6 +33,7 @@ class MSLoginCallbackView(View):
         )
         access_token = result.get('access_token')
         if access_token is None:
+            logger.warning(f'[MICROSOFT CALLBACK] No access token in result: {result}')
             return HttpResponseBadRequest()
         user_info = get_user_info(access_token)
         return JsonResponse(user_info)
