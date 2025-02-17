0951335000d25a2bb6914d6dad19df888426aace
Siddhartha Pratim Dutta <2897074D@student.gla.ac.uk>
2025-02-17 08:39:19 +0000

    Add Token Refresh View

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views import MicrosoftAuthURLAPIView, MicrosoftLoginCallbackAPIView
 
 
 urlpatterns = [
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
     path('microsoft/url/', MicrosoftAuthURLAPIView.as_view(), name='ms-auth-url'),
     path(
         'microsoft/callback/',
