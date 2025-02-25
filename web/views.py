be73c469f1576eb5bbe128697436a82b66ba5e62
Siddhartha Pratim Dutta <2897074D@student.gla.ac.uk>
2025-02-25 17:55:25 +0000

    Clean Code

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.mixins import ListModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
